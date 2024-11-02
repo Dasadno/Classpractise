0500f8e8dc279b65a415aa7d9de96e5e6dcaa1a7
Dasadno <Yakaleks96@gmail.com>
2024-11-02 22:24:35 +0500

    Unluck

--- Classpractise/main.cpp
+++ Classpractise/main.cpp
@@ -1,6 +1,51 @@
-class human {
-public:
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <Windows.h>
+
+class storage {
+public: 
+	
 
 
 
 };
+void add_product(int idorder, int* idarr, int arrsize);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+	unsigned int arrsize = 3;
+
+
+	int* arr1 = new int[arrsize];
+	for (int i = 0; i < arrsize; i++)
+	{
+		
+		arr1[i] = rand() % 10 + 1;
+	}
+	add_product(12, arr1, arrsize);
+	std::cout << arr1;
+
+
+
+	return 0;
+}
+
+void add_product(int idorder, int *idarr, int arrsize) { // idorder needs to be defined in other function, that works with storage refresh, but it's just a test xd :)
+	int *newidarr = new int[arrsize++];
+
+	for (int i = 0; i < arrsize; i++)
+	{
+		newidarr[i] = idarr[i];
+	}
+	newidarr[arrsize] = idorder;
+
+	for (int i = 0; i < arrsize++; i++)
+	{
+		idarr[i] = newidarr[i];
+	}
+
+}
