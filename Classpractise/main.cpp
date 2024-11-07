ab93b4e6a77bc3043595dea315a1771c141ece06
Dasadno <Yakaleks96@gmail.com>
2024-11-07 22:06:45 +0500

    finally, we won

--- Classpractise/main.cpp
+++ Classpractise/main.cpp
@@ -1,43 +1,44 @@
-#include <vector>
-#include <cstdlib>
 #include <iostream>
-#include <Windows.h>
+#include <windows.h>
+#include <cstdlib>
+#include <vector>
 
+void add_product(int idorder, int*& idarr, int& arrsize);
 
-template <typename arr> arr* add_product(int idorder, int* idarr, int arrsize);
 
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	srand(time(NULL));
-	unsigned int arrsize = 3;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int arrsize = 5;
 
 
-	int* arr1 = new int[arrsize];
-	arr1[0] = 1;
-	arr1[1] = 2;
-	arr1[2] = 3;
-	unsigned int id = 95214214;
-	add_product(id, arr1, arrsize);
-	for (int i = 0; i < arrsize; i++)
-	{
-		std::cout <<  << " ";
-	}
+    int* arr1 = new int[arrsize] {96015321, 96143013, 95121233, 91512491, 91024195};
+
+    int id = 95214214;
+    add_product(id, arr1, arrsize);
+    for (int i = 0; i < arrsize; i++)
+    {
+        std::cout << arr1[i] << "\n";
+    }
 
 
-
-	return 0;
+    delete[] arr1;
+    return 0;
 }
-template <typename arr>
-arr* add_product(int idorder, int* idarr, int arrsize) { // idorder needs to be defined in other function, that works with storage refresh
-	arr* newidarr = new arr[arrsize++];
 
-	for (int i = 0; i < arrsize; i++)
-	{
-		newidarr[i] = idarr[i];
-	}
-	newidarr[arrsize + 2] = idorder;
+void add_product(int idorder, int*& idarr, int& arrsize) {
 
-	return newidarr;
+    int new_arr_size = arrsize + 1;
+    int* newidarr = new int[new_arr_size];
+
+    for (int i = 0; i < arrsize; i++) {
+        newidarr[i] = idarr[i];
+    }
+
+    newidarr[arrsize] = idorder;
+    std::swap(idarr, newidarr);
+
+    arrsize = new_arr_size;
+    delete[] newidarr;
 }
