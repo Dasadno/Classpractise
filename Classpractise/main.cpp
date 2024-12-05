d39a26f48e3bf0cc36925ec58611fe9d131f52e0
Dasadno <Yakaleks96@gmail.com>
2024-12-06 03:42:16 +0500

    look cool

--- Classpractise/main.cpp
+++ Classpractise/main.cpp
@@ -1,44 +1,14 @@
 #include <iostream>
-#include <windows.h>
-#include <cstdlib>
-#include <vector>
-
-void add_product(int idorder, int*& idarr, int& arrsize);
+#include <Windows.h>
 
 
-int main()
-{
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    int arrsize = 5;
-
-
-    int* arr1 = new int[arrsize] {96015321, 96143013, 95121233, 91512491, 91024195};
-
-    int id = 95214214;
-    add_product(id, arr1, arrsize);
-    for (int i = 0; i < arrsize; i++)
-    {
-        std::cout << arr1[i] << "\n";
-    }
-
-
-    delete[] arr1;
-    return 0;
-}
-
-void add_product(int idorder, int*& idarr, int& arrsize) {
-
-    int new_arr_size = arrsize + 1;
-    int* newidarr = new int[new_arr_size];
-
-    for (int i = 0; i < arrsize; i++) {
-        newidarr[i] = idarr[i];
-    }
-
-    newidarr[arrsize] = idorder;
-    std::swap(idarr, newidarr);
-
-    arrsize = new_arr_size;
-    delete[] newidarr;
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string phraze = "Какой прекрасный текст, однако.";
+	for (int i = 0; i < phraze.size(); i++)
+	{
+		std::cout << phraze[i];
+		Sleep(20);
+	}
 }
